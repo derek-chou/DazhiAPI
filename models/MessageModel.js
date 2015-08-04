77f56da054da65e909d4fa3d0c53e87b60952a6c
Derek Chou <derek306@gmail.com.tw>
2015-08-04 16:22:59 +0800

    1. add message 2. update message read 3. query message

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -11,4 +11,23 @@ MessageModel.prototype.query = function(type, id, seq, callback) {
   });
 };
 
+MessageModel.prototype.updateRead = function(type, id, seq, callback) {
+  var db = new dbBase();
+  db._query('SELECT "spSetMessageRead"($1, $2, $3);', [type, id, seq]).then(function(result){
+    callback( true, result.rows );
+  }).catch(function(err){
+    callback( false, err );
+  });
+};
+
+MessageModel.prototype.add = function(from_type, from_id, to_type, to_id, msg, callback) {
+  var db = new dbBase();
+  db._query('SELECT "spAddMessage"($1, $2, $3, $4, $5);', 
+  	[from_type, from_id, to_type, to_id, msg]).then(function(result){
+    callback( true, result.rows );
+  }).catch(function(err){
+    callback( false, err );
+  });
+};
+
 module.exports = MessageModel;
--- routes/message.js
+++ routes/message.js
@@ -17,4 +17,54 @@ router.route('/')
   });
 })
 
+.put(function(req, res) {
+  //console.log(req.body);
+  //console.log(Object.keys(req.body).length);
+
+  var type, id, seq;
+  type = req.body['type']; 
+  id = req.body['id']; 
+  seq = req.body['seq']; 
+
+  var messageModel = new MessageModel();
+  messageModel.updateRead(type, id, seq, 
+    function(ret, data){
+    if( ret )
+      res.json({
+        result: 'success',
+      });
+    else
+      res.json({
+        result: 'fail',
+        message: data
+      });
+  });
+})
+
+.post(function(req, res) {
+  //console.log(req.body);
+  //console.log(Object.keys(req.body).length);
+
+  var from_type, from_id, to_type, to_id, msg;
+  from_type = req.body['from_type']; 
+  from_id = req.body['from_id']; 
+  to_type = req.body['to_type']; 
+  to_id = req.body['to_id']; 
+  msg = req.body['msg']; 
+
+  var messageModel = new MessageModel();
+  messageModel.add(from_type, from_id, to_type, to_id, msg, 
+    function(ret, data){
+    if( ret )
+      res.json({
+        result: 'success'
+      });
+    else
+      res.json({
+        result: 'fail',
+        message: data
+      });
+  });
+})
+
 module.exports = router;
