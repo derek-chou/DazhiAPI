7b19131a8622a32bd191967326f1eb9a41c101dc
Derek Chou <derek306@gmail.com.tw>
2015-08-11 11:38:30 +0800

    1. query product by ID

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -11,6 +11,15 @@ ProductModel.prototype.query = function(seq, callback) {
   });
 };
 
+ProductModel.prototype.queryByID = function(type, id, seq, callback) {
+  var db = new dbBase();
+  db._query('select * from "spGetProductByID"($1, $2, $3)', [type, id, seq]).then(function(result){
+    callback( true, result.rows );
+  }).catch(function(err){
+    callback( false, err );
+  });
+};
+
 ProductModel.prototype.add = function(type, id, title, content, price, currency, callback) {
   var db = new dbBase();
   db._query('select * from "spAddProduct"($1, $2, $3, $4, $5, $6)', 
