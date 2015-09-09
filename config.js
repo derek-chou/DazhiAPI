1852dd16038f75d611fe291e0dfea9e9abf7dcdb
Derek Chou <derek306@gmail.com.tw>
2015-09-09 17:01:44 +0800

    no message

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.pgConnectStr = "postgres://postgres:postgres@127.0.0.1/Hello";
+config.debug = true;
+config.urlPrefix = "http://52.88.165.206:8080/";
+
+module.exports = config;
