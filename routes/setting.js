953206b0f74f18114bf3c89ffba4ce525f81f44c
Derek Chou <derek306@gmail.com.tw>
2016-01-13 13:46:53 +0800

    1. 增加相關的html(隱私政策, 關於, 規範...)

--- routes/setting.js
+++ routes/setting.js
@@ -5,4 +5,31 @@ router.get('/', function(req, res, next) {
   res.render('setting', { title: 'Express' });
 });
 
+function getLangPath(lang) {
+  if (lang === "zh_TW")
+  	return "tw";
+  else if (lang === "zh_CN")
+  	return "cn";
+  else
+  	return "en";
+}
+
+router.get('/about', function(req, res, next) {
+  var lang = getLangPath(req.query.lang);
+
+  res.render(lang+'/about');
+});
+
+router.get('/rules', function(req, res, next) {
+  var lang = getLangPath(req.query.lang);
+
+  res.render(lang+'/rules');
+});
+
+router.get('/privacy', function(req, res, next) {
+  var lang = getLangPath(req.query.lang);
+
+  res.render(lang+'/privacy');
+});
+
 module.exports = router;
